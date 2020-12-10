ca467b927289e5e59d7a5689b5eef0eeed6c7e2e
Daniel Nieto-González <nieto.daniel221@gmail.com>
2020-12-09 19:10:33 -0500

    Tranlations fixed

--- src/pageparts/cv/MyInfo.js
+++ src/pageparts/cv/MyInfo.js
@@ -8,7 +8,7 @@ const MyInfo = (tCV) => {
     const intro = <span className="text-center">
         {SUB("h1","subtitle",tCV("namex"))}
         <span className="newline"><span className="text-center nolineP littlespace mt-2">{ EmailLink() }{<span className="pl-1">   </span>}{ TelLink() }</span></span>
-        <span className="newline"><span className="text-center nolineP littlespace"><p><i className="las la-map-marker la-lg mr-2"></i></p><p>Remote work / Bogotá, Colombia</p></span></span>
+        <span className="newline"><span className="text-center nolineP littlespace"><p><i className="las la-map-marker la-lg mr-2"></i></p><p>{tCV("location")}</p></span></span>
         <span className="newline"><span className="text-center nolineP littlespace"><p><i className="las la-globe-americas la-lg mr-2"></i></p>{PRM("","[https://nietodaniel.github.io](https://nietodaniel.github.io)")}</span></span>
         <span className="newline"><span className="text-center nolineP littlespace"><p><i className="la la-linkedin la-lg mr-2"></i></p>{PRM("","[https://www.linkedin.com/in/nietodaniel-apps/](https://www.linkedin.com/in/nietodaniel-apps/)")}</span></span>
     </span>
