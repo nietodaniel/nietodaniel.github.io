150196c3f2682216f9b8b3a75002747c0ce1cdda
Daniel Nieto-González <nieto.daniel221@gmail.com>
2020-12-15 12:59:11 -0500

    content update

--- src/pageparts/software/SoftwarePP.js
+++ src/pageparts/software/SoftwarePP.js
@@ -4,7 +4,13 @@ const SoftwarePP = (t) => {
 
     const contentArr =[
         "content0",
-        "content1"
+        "content1",
+        "content2",
+        "content3",
+        "content4",
+        "content5",
+        "content6",
+        "content7"
       ]
     
       const txt = TransMultiPRM(t,"h2","subtitle","","pl-2 nospace",contentArr)
